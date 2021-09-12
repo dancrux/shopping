ebae05cb0938e45c3d3954b948525d30584931ed
dancrux <nwogwugwudaniel@gmail.com>
2021-09-13 00:17:50 +0100

    set up App bar

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: createAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar createAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.arrow_back_outlined),
+        color: kTextColor,
+      ),
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.search_outlined),
+          color: kTextColor,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.shopping_cart_outlined),
+          color: kTextColor,
+        ),
+        SizedBox(
+          width: kDefaultPaddin / 2,
+        )
+      ],
+    );
+  }
+}
